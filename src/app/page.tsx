8881b576956699373f1dfd2e936b1f6212cb30cf
Jared Russell <jaredhrussell2016@gmail.com>
2025-02-21 17:51:37 -0500

    Restructure page and add disable edit functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,37 @@ import { useState } from "react";
 import SpellLevel from "../components/SpellLevel";
 export default function Home() {
 
+  const [disableRemove, setDisableRemove] = useState<boolean>(false);
 
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <h1 className="text-4xl text-center p-8">Spellbook</h1>
-      <div className="flex flex-col items-center justify-center w-[50%] p-8">
-          <SpellLevel level="One" />
-          <SpellLevel level="Two" />
-          <SpellLevel level="Three" />
+      <label className="btn btn-circle swap swap-rotate m-5">
+      {/* this hidden checkbox controls the state */}
+     <input type="checkbox" onChange={() => setDisableRemove(!disableRemove)}/>
+      {/* hamburger icon */}
+        <svg
+    className="swap-off fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 512 512">
+    <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+      </svg>
+      {/* close icon */}
+      <svg
+    className="swap-on fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 512 512">
+    <polygon
+      points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+      </svg>
+      </label>
+      <div className="grid grid-cols-2 gap-8 p-8 ">
+          <SpellLevel level="One" disableRemove={disableRemove}/>
+          <SpellLevel level="Two" disableRemove={disableRemove}/>
+          <SpellLevel level="Three" disableRemove={disableRemove}/>
       </div>
     </div>
   );
--- src/components/SpellLevel.tsx
+++ src/components/SpellLevel.tsx
@@ -3,43 +3,54 @@ import { useState } from "react";
 
 interface SpellLevelProps {
     level: string;
+    disableRemove: boolean;
   }
 
-const SpellLevel: React.FC<SpellLevelProps> = ({level}) => {
+const SpellLevel: React.FC<SpellLevelProps> = ({level, disableRemove}) => {
     const [lvl, setLvl] = useState<number[]>([1]);
     const addNewSlot = () => {
-        setLvl([...lvl, Date.now()]);
+        if (lvl.length != 4 && !disableRemove){
+            setLvl([...lvl, Date.now()]);
+        }
       };
       const removeSlot = (index: number) => {
+        if (lvl.length != 1 && !disableRemove){
         setLvl(lvl.filter((slot) => slot !== index));
+        }
       };
     return (
-        <div className="flex flex-row items-center justify-center m-4">
-            <button onClick={() => addNewSlot()} className="btn btn-primary m-3">
-            Add New Level {level} Slot
+        <div className="flex flex-col items-center">
+        <h1 className="text-2xl text-black">Level {level}</h1>
+        <div className="flex flex-row  justify-center m-4">
+            
+            <button onClick={() => addNewSlot()} className={disableRemove? "hidden": "btn btn-primary m-3"}>
+                Add new slot
                   </button>
                   <div className="flex flex-col items-center">
                   {lvl.map((slot) => (
             <div key={slot} className="m-0 p-4 rounded-lg shadow-md border-black border-2 flex flex-row items-center">
               <input type="checkbox" className="checkbox m-1 checkbox-primary" />
               <input type="text" className="input input-bordered input-primary m-1 text-white" placeholder="Spell..." />
-              <button onClick={() => removeSlot(slot)} className="btn btn-circle btn-sm btn-error">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-              </button>
+              <div className={disableRemove ? "hidden w-6" : ""}>
+                  <button onClick={() => removeSlot(slot)} className="btn btn-circle btn-sm btn-error">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                  </button>
+              </div>
             </div>
                   ))}
                   </div>
+        </div>
         </div>
     )}
 
