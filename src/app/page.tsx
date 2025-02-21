f06203f435b9dea453de3862399b130686333d8f
Jared Russell <jaredhrussell2016@gmail.com>
2025-02-21 17:11:55 -0500

    Add title (test prod deploymen)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <h1 className="text-4xl text-center p-8">Spellbook</h1>
       <div className="flex flex-col items-center justify-center w-[50%] p-8">
           <SpellLevel level="One" />
           <SpellLevel level="Two" />
